dbc7ca456afc1483c6aa0027b10b6cc9a0ea50ab
Николай Ставинов <nstavinov@gmail.com>
2026-03-07 02:30:52 +0300

    Описание изменений

---
+++ config_local.py
@@ -0,0 +1,14 @@
+# Telegram уведомления
+TG_TOKEN = "6069819791:AAH9IdNqbwE8JKI5GrelMfxTi4t2gSDB1Xk"
+TG_CHAT_ID = "230919946"
+
+
+
+# -----------------------------
+# Binance API настройки
+# -----------------------------
+api_key = "BXswIOpjuPusB4iuBeKMUNIeh1KqyIRjVK7qwQFg4rj7WxSzK4fkCTfqMEx0DU12"
+api_secret = "lIlBDn2TNBT2hWUqxQCEhjrOYvzPZVyHyzGsDfCpuz8Yw6Jz2IIuHCWrYJZxcLpo"
+testnet = True              # Использовать тестовую сеть Binance Futures
+futures_type = "USD-M"      # USD-M Futures
+leverage = 20               # Кредитное плечо
